2964e2127fdb9cce913d62b985f4aba4912b6cc6
mohammadjavad <mohamad2134javad@gmail.com>
2026-08-23 18:27:22 +0330

    add style for product

--- src/components/Product/Products.jsx
+++ src/components/Product/Products.jsx
@@ -1,16 +1,127 @@
 function Products() {
   return (
-    <div className="max-w-content mx-auto px-8">
+    <div className="max-w-content mx-auto px-8 mt-20">
       <h3 className="text-coffee text-2xl font-bold">پر فروش ترین</h3>
-      <div className="grid grid-cols-4 gap-2">
-        <div className="">
+      <div className="grid grid-cols-4 gap-4 mt-[-1rem]">
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
           <img src="/img/pro/image.png" alt="" />
-          <div>
-            <span>قهوه</span>
-            <span>قهوه 50% | شیر 50%</span>
-            <span>460000</span>
+          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+            <span className=" text-text-box font-bold text-xl">لاته</span>
+            <span className="text-text-box-black text-sm">
+              قهوه 50% | شیر 50%
+            </span>
+            <span className="text-text-box font-bold text-lg">
+              460000 تومان
+            </span>
           </div>
-          <button>سفارش</button>
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+            سفارش
+          </button>
+        </div>
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
+          <img src="/img/pro/image.png" alt="" />
+          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+            <span className=" text-text-box font-bold text-xl">لاته</span>
+            <span className="text-text-box-black text-sm">
+              قهوه 50% | شیر 50%
+            </span>
+            <span className="text-text-box font-bold text-lg">
+              460000 تومان
+            </span>
+          </div>
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+            سفارش
+          </button>
+        </div>
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
+          <img src="/img/pro/image.png" alt="" />
+          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+            <span className=" text-text-box font-bold text-xl">لاته</span>
+            <span className="text-text-box-black text-sm">
+              قهوه 50% | شیر 50%
+            </span>
+            <span className="text-text-box font-bold text-lg">
+              460000 تومان
+            </span>
+          </div>
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+            سفارش
+          </button>
+        </div>
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
+          <img src="/img/pro/image.png" alt="" />
+          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+            <span className=" text-text-box font-bold text-xl">لاته</span>
+            <span className="text-text-box-black text-sm">
+              قهوه 50% | شیر 50%
+            </span>
+            <span className="text-text-box font-bold text-lg">
+              460000 تومان
+            </span>
+          </div>
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+            سفارش
+          </button>
+        </div>
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
+          <img src="/img/pro/image.png" alt="" />
+          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+            <span className=" text-text-box font-bold text-xl">لاته</span>
+            <span className="text-text-box-black text-sm">
+              قهوه 50% | شیر 50%
+            </span>
+            <span className="text-text-box font-bold text-lg">
+              460000 تومان
+            </span>
+          </div>
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+            سفارش
+          </button>
+        </div>
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
+          <img src="/img/pro/image.png" alt="" />
+          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+            <span className=" text-text-box font-bold text-xl">لاته</span>
+            <span className="text-text-box-black text-sm">
+              قهوه 50% | شیر 50%
+            </span>
+            <span className="text-text-box font-bold text-lg">
+              460000 تومان
+            </span>
+          </div>
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+            سفارش
+          </button>
+        </div>
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
+          <img src="/img/pro/image.png" alt="" />
+          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+            <span className=" text-text-box font-bold text-xl">لاته</span>
+            <span className="text-text-box-black text-sm">
+              قهوه 50% | شیر 50%
+            </span>
+            <span className="text-text-box font-bold text-lg">
+              460000 تومان
+            </span>
+          </div>
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+            سفارش
+          </button>
+        </div>
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
+          <img src="/img/pro/image.png" alt="" />
+          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+            <span className=" text-text-box font-bold text-xl">لاته</span>
+            <span className="text-text-box-black text-sm">
+              قهوه 50% | شیر 50%
+            </span>
+            <span className="text-text-box font-bold text-lg">
+              460000 تومان
+            </span>
+          </div>
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+            سفارش
+          </button>
         </div>
       </div>
     </div>
