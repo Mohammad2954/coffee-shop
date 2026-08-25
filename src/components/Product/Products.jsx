e037390d4a29cd878ff09bd477472b9b7912d75c
mohammadjavad <mohamad2134javad@gmail.com>
2026-08-25 16:57:25 +0330

    fix style bug for product

--- src/components/Product/Products.jsx
+++ src/components/Product/Products.jsx
@@ -2,10 +2,14 @@ function Products() {
   return (
     <div className="max-w-content mx-auto px-8 mt-20">
       <h3 className="text-coffee text-2xl font-bold">پر فروش ترین</h3>
-      <div className="grid grid-cols-4 gap-4 mt-[-1rem]">
-        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
-          <img src="/img/pro/image.png" alt="" />
-          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+      <div className="grid  grid-cols- lg:grid-cols-5 sm:grid-cols-3 gap-4 mt-[-1rem]">
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-6 sm:mt-12 sm:block flex justify-between items-center">
+          <img
+            src="/img/pro/image.jpg"
+            alt=""
+            className="sm:w-full w-1/2 rounded-r-2xl sm:rounded-t-2xl sm:rounded-b-none"
+          />
+          <div className="w-1/2  flex items-center sm:w-full sm:jutify-centesr flex-col gap-2 mt-3 mb-8 ">
             <span className=" text-text-box font-bold text-xl">لاته</span>
             <span className="text-text-box-black text-sm">
               قهوه 50% | شیر 50%
@@ -14,13 +18,17 @@ function Products() {
               460000 تومان
             </span>
           </div>
-          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/6 sm:left-0 sm:right-0 w-24 mx-auto">
             سفارش
           </button>
         </div>
-        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
-          <img src="/img/pro/image.png" alt="" />
-          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-6 sm:mt-12 sm:block flex justify-between items-center">
+          <img
+            src="/img/pro/image.jpg"
+            alt=""
+            className="sm:w-full w-1/2 rounded-r-2xl sm:rounded-t-2xl sm:rounded-b-none"
+          />
+          <div className="w-1/2  flex items-center sm:w-full sm:jutify-centesr flex-col gap-2 mt-3 mb-8 ">
             <span className=" text-text-box font-bold text-xl">لاته</span>
             <span className="text-text-box-black text-sm">
               قهوه 50% | شیر 50%
@@ -29,13 +37,17 @@ function Products() {
               460000 تومان
             </span>
           </div>
-          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/6 sm:left-0 sm:right-0 w-24 mx-auto">
             سفارش
           </button>
         </div>
-        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
-          <img src="/img/pro/image.png" alt="" />
-          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-6 sm:mt-12 sm:block flex justify-between items-center">
+          <img
+            src="/img/pro/image.jpg"
+            alt=""
+            className="sm:w-full w-1/2 rounded-r-2xl sm:rounded-t-2xl sm:rounded-b-none"
+          />
+          <div className="w-1/2  flex items-center sm:w-full sm:jutify-centesr flex-col gap-2 mt-3 mb-8 ">
             <span className=" text-text-box font-bold text-xl">لاته</span>
             <span className="text-text-box-black text-sm">
               قهوه 50% | شیر 50%
@@ -44,13 +56,17 @@ function Products() {
               460000 تومان
             </span>
           </div>
-          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/6 sm:left-0 sm:right-0 w-24 mx-auto">
             سفارش
           </button>
         </div>
-        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
-          <img src="/img/pro/image.png" alt="" />
-          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-6 sm:mt-12 sm:block flex justify-between items-center">
+          <img
+            src="/img/pro/image.jpg"
+            alt=""
+            className="sm:w-full w-1/2 rounded-r-2xl sm:rounded-t-2xl sm:rounded-b-none"
+          />
+          <div className="w-1/2  flex items-center sm:w-full sm:jutify-centesr flex-col gap-2 mt-3 mb-8 ">
             <span className=" text-text-box font-bold text-xl">لاته</span>
             <span className="text-text-box-black text-sm">
               قهوه 50% | شیر 50%
@@ -59,13 +75,17 @@ function Products() {
               460000 تومان
             </span>
           </div>
-          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/6 sm:left-0 sm:right-0 w-24 mx-auto">
             سفارش
           </button>
         </div>
-        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
-          <img src="/img/pro/image.png" alt="" />
-          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-6 sm:mt-12 sm:block flex justify-between items-center">
+          <img
+            src="/img/pro/image.jpg"
+            alt=""
+            className="sm:w-full w-1/2 rounded-r-2xl sm:rounded-t-2xl sm:rounded-b-none"
+          />
+          <div className="w-1/2  flex items-center sm:w-full sm:jutify-centesr flex-col gap-2 mt-3 mb-8 ">
             <span className=" text-text-box font-bold text-xl">لاته</span>
             <span className="text-text-box-black text-sm">
               قهوه 50% | شیر 50%
@@ -74,13 +94,17 @@ function Products() {
               460000 تومان
             </span>
           </div>
-          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/6 sm:left-0 sm:right-0 w-24 mx-auto">
             سفارش
           </button>
         </div>
-        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
-          <img src="/img/pro/image.png" alt="" />
-          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-6 sm:mt-12 sm:block flex justify-between items-center">
+          <img
+            src="/img/pro/image.jpg"
+            alt=""
+            className="sm:w-full w-1/2 rounded-r-2xl sm:rounded-t-2xl sm:rounded-b-none"
+          />
+          <div className="w-1/2  flex items-center sm:w-full sm:jutify-centesr flex-col gap-2 mt-3 mb-8 ">
             <span className=" text-text-box font-bold text-xl">لاته</span>
             <span className="text-text-box-black text-sm">
               قهوه 50% | شیر 50%
@@ -89,13 +113,17 @@ function Products() {
               460000 تومان
             </span>
           </div>
-          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/6 sm:left-0 sm:right-0 w-24 mx-auto">
             سفارش
           </button>
         </div>
-        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
-          <img src="/img/pro/image.png" alt="" />
-          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-6 sm:mt-12 sm:block flex justify-between items-center">
+          <img
+            src="/img/pro/image.jpg"
+            alt=""
+            className="sm:w-full w-1/2 rounded-r-2xl sm:rounded-t-2xl sm:rounded-b-none"
+          />
+          <div className="w-1/2  flex items-center sm:w-full sm:jutify-centesr flex-col gap-2 mt-3 mb-8 ">
             <span className=" text-text-box font-bold text-xl">لاته</span>
             <span className="text-text-box-black text-sm">
               قهوه 50% | شیر 50%
@@ -104,13 +132,17 @@ function Products() {
               460000 تومان
             </span>
           </div>
-          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/6 sm:left-0 sm:right-0 w-24 mx-auto">
             سفارش
           </button>
         </div>
-        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-12">
-          <img src="/img/pro/image.png" alt="" />
-          <div className=" flex items-center justify-center flex-col gap-2 mt-3 mb-8">
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-6 sm:mt-12 sm:block flex justify-between items-center">
+          <img
+            src="/img/pro/image.jpg"
+            alt=""
+            className="sm:w-full w-1/2 rounded-r-2xl sm:rounded-t-2xl sm:rounded-b-none"
+          />
+          <div className="w-1/2  flex items-center sm:w-full sm:jutify-centesr flex-col gap-2 mt-3 mb-8 ">
             <span className=" text-text-box font-bold text-xl">لاته</span>
             <span className="text-text-box-black text-sm">
               قهوه 50% | شیر 50%
@@ -119,7 +151,45 @@ function Products() {
               460000 تومان
             </span>
           </div>
-          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/3">
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/6 sm:left-0 sm:right-0 w-24 mx-auto">
+            سفارش
+          </button>
+        </div>
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-6 sm:mt-12 sm:block flex justify-between items-center">
+          <img
+            src="/img/pro/image.jpg"
+            alt=""
+            className="sm:w-full w-1/2 rounded-r-2xl sm:rounded-t-2xl sm:rounded-b-none"
+          />
+          <div className="w-1/2  flex items-center sm:w-full sm:jutify-centesr flex-col gap-2 mt-3 mb-8 ">
+            <span className=" text-text-box font-bold text-xl">لاته</span>
+            <span className="text-text-box-black text-sm">
+              قهوه 50% | شیر 50%
+            </span>
+            <span className="text-text-box font-bold text-lg">
+              460000 تومان
+            </span>
+          </div>
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/6 sm:left-0 sm:right-0 w-24 mx-auto">
+            سفارش
+          </button>
+        </div>
+        <div className="border border-box-border rounded-2xl relative bg-box-bg mt-6 sm:mt-12 sm:block flex justify-between items-center">
+          <img
+            src="/img/pro/image.jpg"
+            alt=""
+            className="sm:w-full w-1/2 rounded-r-2xl sm:rounded-t-2xl sm:rounded-b-none"
+          />
+          <div className="w-1/2  flex items-center sm:w-full sm:jutify-centesr flex-col gap-2 mt-3 mb-8 ">
+            <span className=" text-text-box font-bold text-xl">لاته</span>
+            <span className="text-text-box-black text-sm">
+              قهوه 50% | شیر 50%
+            </span>
+            <span className="text-text-box font-bold text-lg">
+              460000 تومان
+            </span>
+          </div>
+          <button className="bg-box-btn rounded-full px-6 py-2 text-coffe absolute bottom-[-20px] left-1/6 sm:left-0 sm:right-0 w-24 mx-auto">
             سفارش
           </button>
         </div>
