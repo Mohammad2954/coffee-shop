db11b39cc24d4766c382e7e4dd01b8b2880de998
mohammadjavad <mohamad2134javad@gmail.com>
2026-09-02 19:00:26 +0330

    complet 404 page for mistak path

---
+++ src/pages/Page404.jsx
@@ -0,0 +1,17 @@
+import Button from "../components/Button/Button";
+
+function Page404() {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center gap-8 sm:gap-20 bg-blue-300">
+      <div className="mt-8">
+        <h1 className="font-bold-AvenirNextCyr text-6xl">Oops!</h1>
+        <h3 className="text-center mt-4">You are lost</h3>
+      </div>
+
+      <img className="px-16" src="/img/404/405.png" alt="" />
+      <Button />
+    </div>
+  );
+}
+
+export default Page404;
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,5 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home";
+import Page404 from "../pages/Page404";
 
-const route = createBrowserRouter([{ path: "/home", element: <Home /> }]);
+const route = createBrowserRouter([
+  { path: "/home", element: <Home /> },
+  { path: "/*", element: <Page404 /> },
+]);
 export default route;
