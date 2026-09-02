35fe0239adbcd816539c485cd71c61ae3be5b7e6
mohammadjavad <mohamad2134javad@gmail.com>
2026-09-02 13:00:26 +0330

    add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Category />
+      <Products />
+      <MainBox />
+      <PopProduct />
+      <Comment />
+    </>
+  );
+}
+
+export default Home;
