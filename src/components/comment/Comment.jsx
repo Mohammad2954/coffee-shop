3d2f553195ead6e4e400c4272f91624feb17b24a
mohammadjavad <mohamad2134javad@gmail.com>
2026-09-02 19:07:09 +0330

    fixed bug comment in small screen

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -2,7 +2,7 @@ import { commentsData } from "../commendata.js";
 import Comments from "./Comments.jsx";
 function Comment() {
   return (
-    <div className=" mt-32 max-w-content mx-auto px-8 grid grid-cols-3 gap-3">
+    <div className=" mt-32 max-w-content mx-auto px-8 grid grid-cols-1 sm:grid-cols-3 gap-3">
       {commentsData.map((com) => {
         return <Comments {...com} />;
       })}
