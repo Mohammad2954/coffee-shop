8663f6e735fee2e1d631bd7bb58f9d1d63eeba7d
mohammadjavad <mohamad2134javad@gmail.com>
2026-09-06 23:35:26 +0330

    feat:add signup page template

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -4,6 +4,7 @@ import Page404 from "../pages/Page404";
 import RootLayout from "../components/RootLayout/RootLayout";
 import Category from "../pages/Category";
 import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
 const route = createBrowserRouter([
   {
     path: "/",
@@ -12,6 +13,7 @@ const route = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: "category", element: <Category /> },
       { path: "login", element: <Login /> },
+      { path: "signup", element: <SignUp /> },
       {
         path: "/*",
         element: <Page404 />,
