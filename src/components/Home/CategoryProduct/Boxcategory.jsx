4075f8b90e999ff96c68ed30fa352faec43fba7c
mohammadjavad <mohamad2134javad@gmail.com>
2026-09-09 23:08:16 +0330

    style: change category style

--- src/components/Home/CategoryProduct/Boxcategory.jsx
+++ src/components/Home/CategoryProduct/Boxcategory.jsx
@@ -1,8 +1,8 @@
 function Boxcategory({ title, svg }) {
   return (
-    <div className="bg-[#775142] rounded-lg  flex flex-col items-center justify-center py-10 mt-8 shadow-2xl">
+    <div className=" rounded-lg  flex flex-col items-center justify-center  lg:text-2xl">
       {svg}
-      <p className="text-[#F8E4BE]">{title}</p>
+      <p className="text-[#F8E4BE] text-[10px] lg:text-lg">{title}</p>
     </div>
   );
 }
