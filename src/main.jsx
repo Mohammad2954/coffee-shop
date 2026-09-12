07ec49390ca9fb0f28a0c55a6c3be32e93722686
mohammadjavad <mohamad2134javad@gmail.com>
2026-09-12 12:00:31 +0330

    add react-hot-tost

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,5 +18,6 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <Toaster position="top-left" reverseOrder={false} />
   </QueryClientProvider>,
 );
