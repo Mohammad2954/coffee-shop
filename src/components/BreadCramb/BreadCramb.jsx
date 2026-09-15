22caefb3e0089910b666482af2c9e26b80b2d87f
mohammadjavad <mohamad2134javad@gmail.com>
2026-09-15 23:39:50 +0330

    feat:added bread crumb logeic and show

--- src/components/BreadCramb/BreadCramb.jsx
+++ src/components/BreadCramb/BreadCramb.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import { useMatch, useMatches } from "react-router";
+
 function BreadCramb() {
+  const findCat = useMatches();
+  let pathfind = findCat
+    .map((e) => {
+      switch (e.pathname) {
+        case "/":
+          return "خانه";
+        case "/blog":
+          return "وبلاگ";
+        case "/populer":
+          return "مورد علاقه ها";
+        case "/cart":
+          return "سبد خرید";
+        case "/about":
+          return "درباره ما";
+        case "/contact-us":
+          return "ارتباط با ما";
+
+        default:
+          break;
+      }
+    })
+    .filter((e) => e !== undefined);
+
   return (
-    <div className="w-fit py-1 px-4 bg-coffee-bg rounded-full mr-2 mb-4">
-      <p className="text-sm text-text-header ">خانه/دسته بندی</p>
+    <div className="w-fit py-1 px-4 bg-coffee-bg rounded-full mr-2 mb-4 flex item center gap-2">
+      {pathfind.map((e, index) => {
+        return (
+          <p className="text-sm text-text-header ">
+            {e} {pathfind.length === index + 1 ? "" : "/"}
+          </p>
+        );
+      })}
     </div>
   );
 }
